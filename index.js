090758aeea6e71b9d4bcf7d335d2d84afb40e183
Micah Williamson <micah.williamson@firemon.com>
2014-08-10 13:59:37 -0500

    forgot to add  to index.js

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ var snooze = require('snooze');
 require('./lib/services/lodash');
 require('./lib/services/$validator');
 require('./lib/services/$fs');
+require('./lib/services/$md5');
 
 module.exports = snooze.module('snooze-stdlib');
